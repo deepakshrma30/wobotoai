a990f65de24f2c3e9439ec32ee8acfea764d97ca
deepakshrma30 <deepakrosebball1@gmail.com>
2025-12-10 23:25:42 +0530

    build-fixes

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { useSampleData } from "../service/useSampleData";
 import RingBadge from "./ringBadge";
 
--- src/service/useSampleData.tsx
+++ src/service/useSampleData.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState } from "react";
 import type { DATA } from "../types/types";
 
 export function useSampleData() {
